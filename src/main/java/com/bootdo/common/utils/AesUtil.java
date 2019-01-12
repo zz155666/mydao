3b6397cafbe8b4033a61e229ee5e02f9df60076f
teerus <z83@foxmail.com>
2019-01-12 20:48:06 +0800

    添加aes加密方式

---
+++ src/main/java/com/bootdo/common/utils/AesUtil.java
@@ -0,0 +1,76 @@
+package com.bootdo.common.utils;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @ProjectName: MyDao
+ * @Package: com.bootdo.common.utils
+ * @ClassName: AesUtil
+ * @Description: aes解密与解密工具类
+ * @Author: teerus
+ * @CreateDate: 2019-01-12 20:39
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class AesUtil {
+
+
+
+    public static final String CODE_TYPE = "UTF-8";
+    //加密类型
+    public static final String AES_TYPE = "AES/ECB/PKCS5Padding";
+    /**
+     *
+     * @param cleartext
+     * @return
+     */
+    public static String encrypt(String cleartext) {
+        //加密方式： AES128(CBC/PKCS5Padding) + Base64, 私钥：1111222233334444
+        try {
+            //输入
+            IvParameterSpec zeroIv = new IvParameterSpec(MD5Utils.encrypt("muxiaolin").getBytes());
+            //两个参数，第一个为私钥字节数组， 第二个为加密方式 AES或者DES
+            SecretKeySpec key = new SecretKeySpec(MD5Utils.encrypt("zhangxiangyu").getBytes(), "AES");
+            //实例化加密类，参数为加密方式，要写全
+            Cipher cipher = Cipher.getInstance(AES_TYPE); //PKCS5Padding比PKCS7Padding效率高，PKCS7Padding可支持IOS加解密
+            //初始化，此方法可以采用三种方式，按加密算法要求来添加。（1）无第三个参数（2）第三个参数为SecureRandom random = new SecureRandom();中random对象，随机数。(AES不可采用这种方法)（3）采用此代码中的IVParameterSpec
+            //加密时使用:ENCRYPT_MODE;  解密时使用:DECRYPT_MODE;
+            cipher.init(Cipher.ENCRYPT_MODE, key); //CBC类型的可以在第三个参数传递偏移量zeroIv,ECB没有偏移量
+            //加密操作,返回加密后的字节数组，然后需要编码。主要编解码方式有Base64, HEX, UUE,7bit等等。此处看服务器需要什么编码方式
+            byte[] encryptedData = cipher.doFinal(cleartext.getBytes(CODE_TYPE));
+
+            return new BASE64Encoder().encode(encryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * 解密
+     *
+     * @param encrypted
+     * @return
+     */
+    public static String decrypt(String encrypted) {
+        try {
+            byte[] byteMi = new BASE64Decoder().decodeBuffer(encrypted);
+            IvParameterSpec zeroIv = new IvParameterSpec(MD5Utils.encrypt("muxiaolin").getBytes());
+            SecretKeySpec key = new SecretKeySpec(
+                    MD5Utils.encrypt("zhangxiangyu").getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance(AES_TYPE);
+            //与加密时不同MODE:Cipher.DECRYPT_MODE
+            cipher.init(Cipher.DECRYPT_MODE, key);  //CBC类型的可以在第三个参数传递偏移量zeroIv,ECB没有偏移量
+            byte[] decryptedData = cipher.doFinal(byteMi);
+            return new String(decryptedData, CODE_TYPE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
